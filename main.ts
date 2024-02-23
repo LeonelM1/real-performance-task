9ab50b4633bc0efcbd10c94ff273b2db98b2ec7f
LeonelM1 <156251698+LeonelM1@users.noreply.github.com>
2024-02-23 16:28:17 -0600

    I worked on my sprites mechanics and I worked on my function.

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    shootingDirection = 3
     playerSprite.setImage(assets.image`up`)
     animation.runImageAnimation(
     playerSprite,
@@ -8,27 +9,31 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (playerSprite.image.equals(assets.image`up`)) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . 3 1 1 3 . . . . . . 
-            . . . . . 2 1 1 1 1 2 . . . . . 
-            . . . . . 2 1 1 1 1 2 . . . . . 
-            . . . . . . 3 1 1 3 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, playerSprite, 0, -100)
-    } else if (playerSprite.image.equals(assets.image`right`)) {
-        projectile = sprites.createProjectileFromSprite(img`
+    if (playerSprite.overlapsWith(chest)) {
+        chest.setImage(img`
+            . b b b b b b b b b b b b b b . 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+            b b b b b b b d d b b b b b b b 
+            . b b b b b b c c b b b b b b . 
+            b c c c c c b c c b c c c c c b 
+            b c c c c c c b b c c c c c c b 
+            b c c c c c c c c c c c c c c b 
+            b c c c c c c c c c c c c c c b 
+            b b b b b b b b b b b b b b b b 
+            b e e e e e e e e e e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b c e e e e e e e e e e e e c b 
+            b b b b b b b b b b b b b b b b 
+            . b b . . . . . . . . . . b b . 
+            `)
+        info.changeScoreBy(5)
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (shootingDirection == 1) {
+        fireballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -46,27 +51,8 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, playerSprite, 100, 0)
-    } else if (playerSprite.image.equals(assets.image`down`)) {
-        projectile = sprites.createProjectileFromSprite(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . 3 1 1 3 . . . . . . 
-            . . . . . 2 1 1 1 1 2 . . . . . 
-            . . . . . 2 1 1 1 1 2 . . . . . 
-            . . . . . . 3 1 1 3 . . . . . . 
-            . . . . . . . 2 2 . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, playerSprite, 0, 100)
-    } else {
-        projectile = sprites.createProjectileFromSprite(img`
+    } else if (shootingDirection == 2) {
+        fireballSprite = sprites.createProjectileFromSprite(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -84,29 +70,48 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, playerSprite, -100, 0)
+    } else if (shootingDirection == 3) {
+        fireballSprite = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 3 1 1 3 . . . . . . 
+            . . . . . 2 1 1 1 1 2 . . . . . 
+            . . . . . 2 1 1 1 1 2 . . . . . 
+            . . . . . . 3 1 1 3 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, playerSprite, 0, -100)
+    } else if (shootingDirection == 4) {
+        fireballSprite = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . 3 1 1 3 . . . . . . 
+            . . . . . 2 1 1 1 1 2 . . . . . 
+            . . . . . 2 1 1 1 1 2 . . . . . 
+            . . . . . . 3 1 1 3 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, playerSprite, 0, 100)
     }
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    chest.setImage(img`
-        . b b b b b b b b b b b b b b . 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 4 b 
-        b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
-        b e e 4 4 4 4 4 4 4 4 4 4 e e b 
-        b b b b b b b d d b b b b b b b 
-        . b b b b b b c c b b b b b b . 
-        b c c c c c b c c b c c c c c b 
-        b c c c c c c b b c c c c c c b 
-        b c c c c c c c c c c c c c c b 
-        b c c c c c c c c c c c c c c b 
-        b b b b b b b b b b b b b b b b 
-        b e e e e e e e e e e e e e e b 
-        b e e e e e e e e e e e e e e b 
-        b c e e e e e e e e e e e e c b 
-        b b b b b b b b b b b b b b b b 
-        . b b . . . . . . . . . . b b . 
-        `)
-})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    shootingDirection = 2
     playerSprite.setImage(assets.image`left`)
     animation.runImageAnimation(
     playerSprite,
@@ -119,6 +124,7 @@ info.onCountdownEnd(function () {
 	
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    shootingDirection = 1
     playerSprite.setImage(assets.image`right`)
     animation.runImageAnimation(
     playerSprite,
@@ -128,6 +134,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    shootingDirection = 4
     playerSprite.setImage(assets.image`down`)
     animation.runImageAnimation(
     playerSprite,
@@ -142,7 +149,86 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherS
 info.onLifeZero(function () {
 	
 })
-let projectile: Sprite = null
+function buildRoom (numChests: number) {
+    for (let index = 0; index < numChests; index++) {
+        r = randint(4, 11)
+        c = randint(4, 11)
+        tiles.setTileAt(tiles.getTileLocation(c, r), img`
+            . . b b b b b b b b b b b b . . 
+            . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+            b e e e e e e e e e e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b b b b b b b d d b b b b b b b 
+            c b b b b b b c c b b b b b b c 
+            c c c c c c b c c b c c c c c c 
+            b e e e e e c b b c e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b c e e e e e e e e e e e e c b 
+            b b b b b b b b b b b b b b b b 
+            . b b . . . . . . . . . . b b . 
+            `)
+        rand = randint(0, 3)
+        Direction = "up"
+        if (rand == 0) {
+            r += -1
+            Direction = "up"
+        } else if (rand == 1) {
+            c += 1
+            Direction = "right"
+        } else if (rand == 2) {
+            r += 1
+            Direction = "down"
+        } else {
+            c += -1
+            Direction = "left"
+        }
+        while (!(tiles.tileAtLocationIsWall(tiles.getTileLocation(c, r)) && !(tiles.tileAtLocationEquals(tiles.getTileLocation(c, r), img`
+            . . b b b b b b b b b b b b . . 
+            . b e 4 4 4 4 4 4 4 4 4 4 e b . 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e 4 4 4 4 4 4 4 4 4 4 4 4 e b 
+            b e e 4 4 4 4 4 4 4 4 4 4 e e b 
+            b e e e e e e e e e e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b b b b b b b d d b b b b b b b 
+            c b b b b b b c c b b b b b b c 
+            c c c c c c b c c b c c c c c c 
+            b e e e e e c b b c e e e e e b 
+            b e e e e e e e e e e e e e e b 
+            b c e e e e e e e e e e e e c b 
+            b b b b b b b b b b b b b b b b 
+            . b b . . . . . . . . . . b b . 
+            `)))) {
+            tiles.setTileAt(tiles.getTileLocation(c, r), sprites.dungeon.floorLight0)
+            tiles.setWallAt(tiles.getTileLocation(c, r), true)
+            rand = randint(0, 3)
+            if (rand == 0 && !(Direction == "down")) {
+                Direction += 1
+                Direction = 0
+            } else if (rand == 1 && !(Direction == "left")) {
+                Direction += 1
+                Direction = 0
+            } else if (rand == 2 && !(Direction == "up")) {
+                Direction += 1
+                Direction = 0
+            } else {
+                Direction += 1
+                Direction = 0
+            }
+        }
+    }
+}
+let Direction = ""
+let rand = 0
+let c = 0
+let r = 0
+let fireballSprite: Sprite = null
+let shootingDirection = 0
 let playerSprite: Sprite = null
 let chest: Sprite = null
 chest = sprites.create(img`
@@ -186,9 +272,11 @@ playerSprite.setBounceOnWall(true)
 controller.moveSprite(playerSprite, 100, 100)
 scene.cameraFollowSprite(playerSprite)
 tiles.setCurrentTilemap(tilemap`level1`)
+info.setScore(0)
+info.setLife(3)
 forever(function () {
 	
 })
-game.onUpdateInterval(500, function () {
+game.onUpdateInterval(100, function () {
 	
 })
